40175a10abdae8bd775f9aa0291ce4a866574378
Sudeep <sudeepk.fis@gmail.com>
2024-11-12 17:29:17 +0530

    Added graph

--- graph/graph.c
+++ graph/graph.c
@@ -1,8 +1,9 @@
-// Undirected graph using adjacency matrix
+// Undirected, unweighted graph using adjacency matrix
+
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 10
+#define N 5
 
 void insertEdge (int graph[N][N], int a, int b){
   // Joining a to b
@@ -21,6 +22,30 @@ void deleteEdge (int graph[N][N], int a, int b){
 
 }
 
-void searchEdge (){
+int searchEdge (int graph[N][N], int a, int b){
+  // Checks for the existence of edge u-v
+  return graph[a][b];
+}
 
+void displayGraph (int graph[N][N], int n){
+  for (int i = 0;i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      printf("%d ", graph[i][j]);
+    }
+  puts("");
+  }
+}
+
+int main (){
+  int graph [N][N] = {0};
+
+  // Adding edges
+  insertEdge(graph, 0, 1);
+  insertEdge(graph, 2, 3);
+  insertEdge(graph, 1, 4);
+  insertEdge(graph, 0, 4);
+
+  displayGraph(graph, N);
+
+  return 0;
 }
